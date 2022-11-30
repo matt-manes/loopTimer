b6ff57f5855251798bc8f2ee19b5f15879426165
matt manes <113395539+matt-manes@users.noreply.github.com>
2022-11-30 16:23:46 -0600

    Add files via upload

--- src/loopTimer/loopTimer.py
+++ src/loopTimer/loopTimer.py
@@ -3,12 +3,14 @@ from datetime import datetime
 class Timer:
     """ Simple timer class that tracks total elapsed time\n 
         and average time between calls to 'start' and 'stop'. """
-    def __init__(self, averagingWindowLength=10, subsecondFormat:bool=False):
-        """ If subsecondFormat is True, timer.formatTime() will 
-        will add milliseconds and microseconds to the string it returns."""
+    def __init__(self, averagingWindowLength:int=10, subsecondFormat:bool=False):
+        """ :param averagingWindowLength: Number of start/stop cycles
+        to calculate the average elapsed time with.\n
+        :param subsecondFormat: If True, formatTime() will 
+        include milliseconds and microseconds with its return string."""
         self.startTime = 0
         self.stopTime = 0
-        self.averageTime = 0
+        self.averageElapsedTime = 0
         self.history = []
         self.elapsedTime = 0
         self.averagingWindowLength = averagingWindowLength
@@ -21,30 +23,34 @@ class Timer:
         self.started = True
         
     def stop(self):
-        """ Stop timer. """
+        """ Stop timer.\n
+        Calculates elapsed time and average elapsed time."""
         self.stopTime = datetime.now()
         self.started = False
         self.elapsedTime = (self.stopTime - self.startTime).total_seconds()
         self._saveElapsedTime()
-        self.averageTime = sum(self.history)/(len(self.history))
+        self.averageElapsedTime = sum(self.history)/(len(self.history))
     
     def _saveElapsedTime(self):
-        """ Stores current elapsed time to history and removes oldest elapsed time."""
+        """ Saves current elapsed time to the history buffer 
+        in a FIFO manner."""
         if len(self.history) == 0:
             self.history = [self.elapsedTime]*self.averagingWindowLength
         else:
             self.history.pop(0)
             self.history.append(self.elapsedTime)
     
-    def checkTime(self,format:bool=True):
-        """ Returns current elapsed without stopping the timer."""
+    def checkTime(self, format:bool=True)->float|str:
+        """ Returns current elapsed without stopping the timer.\n
+        :param format: If True, elapsed time is returned as a string. 
+        If False, elapsed time is returned as a float."""
         self.elapsedTime = (datetime.now() - self.startTime).total_seconds()
         return self.formatTime(self.elapsedTime) if format else self.elapsedTime
     
     def _getTimeUnit(self, numSeconds:float, secondsPerUnit:float, unitSuffix:str)->tuple[float, str]:
         """ Determines the number of units in a given number of seconds
         by integer division.\n
-        Returns a tuple containing the left over number of seconds after division 
+        Returns a tuple containing the remaining number of seconds after division 
         as well as the number of units as a string with 'unitSuffix' appended to the string.
         e.g. _getTimeUnit(124, 60, 'm') will return (4, '2m')"""
         numUnits = int(numSeconds/secondsPerUnit)
@@ -55,7 +61,7 @@ class Timer:
             return (numSeconds, '')
     
     def formatTime(self, numSeconds:float)->str:
-        """ Returns numSeconds as a string. """
+        """ Returns numSeconds as a string with units. """
         microsecond = .000001
         millisecond = .001
         second = 1
@@ -78,9 +84,9 @@ class Timer:
                 timeString += f'{unitString} '
         return timeString
     
-    def getStats(self,formatted:bool=True)->str:
+    def getStats(self, format:bool=True)->str:
         """ Returns string for elapsed time and average elapsed time. """
-        if formatted:
-            return f'elapsed time: {self.formatTime(self.elapsedTime)}\naverage elapsed time: {self.formatTime(self.averageTime)}'
+        if format:
+            return f'elapsed time: {self.formatTime(self.elapsedTime)}\naverage elapsed time: {self.formatTime(self.averageElapsedTime)}'
         else:
-            return f'elapsed time: {self.elapsedTime}\naverage elapsed time: {self.averageTime}'
+            return f'elapsed time: {self.elapsedTime}s\naverage elapsed time: {self.averageElapsedTime}s'
